a79cc9f49e49810f1b51ac20f57ef37f831f5733
Scott Weinstein <sweinstein27@gmail.com>
2018-05-18 14:42:01 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,13 +6,13 @@ attr_accessor :name, :artist
 
 def initialize(name)
   @name = name
-end 
+end
 
 def self.new_by_filename(file_name)
 artist = Artist.new(file_name.split(" - ")[0])
 song = Song.new(file_name.split(" - ")[1])
-song.artist = artist 
-song 
+song.artist = artist
+song
 end
 
 end
