3f4d310d3759a3208e3afe31488f5687bc9db61a
Scott Weinstein <sweinstein27@gmail.com>
2018-05-18 13:59:25 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,7 +9,7 @@ def initialize(name)
 end
 
 def self.new_by_filename(file_name)
-binging.pry
+binding.pry
 song = Song.new(file_name.split(" - ")[1])
 artist = Artist.new(file_name.split(" - ")[0])
 end
